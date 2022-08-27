06994a8342c624ef248c44daffe91e544b467833
unknown <git@internode.on.net>
2022-08-28 09:24:26 +0930

    update of files

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'root',
-    password: process.env.password,
+    user: "root",
+    password: "250583",
     database: 'employeeTrackerDB',
 });
 
